53a0a117eb5c53d97b892c1b675491d6a2dbf4f7
Victoria Jackson <jackson.v@mac3108.wehi.edu.au>
2020-01-09 15:40:46 +1100

    Initial commit

---
+++ R/getICD9.R
@@ -0,0 +1,10 @@
+#' Getting ICD9 codes
+#'
+#' This function retrieves ICD9 codes based on string matching.
+#' @param string String to match
+#' @keywords ICD9
+#' @export
+
+getICD9 <- function(string=""){
+    print(string)
+}
